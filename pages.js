8b185004a90781c79da8822bae512ec863286ca7
Joseph Lai <137158285+B1ockchainEngineer@users.noreply.github.com>
2024-01-16 12:12:58 +0800

    desc search

--- pages.js
+++ pages.js
@@ -29,9 +29,9 @@ function myFunction() {
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-        div = tr[i].getElementsByTagName("div")[0];
-        if (div) {
-        txtValue = div.textContent || div.innerText;
+        a = tr[i].getElementsByTagName("a")[0];
+        if (a) {
+        txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             tr[i].style.display = "";
         } else {

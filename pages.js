750821394ed0a0aea5367347a461baf2275502a7
Yung <132829631+Yung729@users.noreply.github.com>
2024-01-16 12:21:21 +0800

    Remove page

--- pages.js
+++ pages.js
@@ -1,26 +1,26 @@
 
-function swapPages(pageNumber) {
+// function swapPages(pageNumber) {
 
-    var allRows = document.querySelectorAll('tr[id^="page_"]');
-    allRows.forEach(function (row) {
-        row.style.display = 'none';
-    });
+//     var allRows = document.querySelectorAll('tr[id^="page_"]');
+//     allRows.forEach(function (row) {
+//         row.style.display = 'none';
+//     });
 
-    // Show rows of the selected page
-    var selectedPageRows = document.querySelectorAll('#page_' + pageNumber);
-    selectedPageRows.forEach(function (row) {
-        row.style.display = 'table-row';
-    });
+//     // Show rows of the selected page
+//     var selectedPageRows = document.querySelectorAll('#page_' + pageNumber);
+//     selectedPageRows.forEach(function (row) {
+//         row.style.display = 'table-row';
+//     });
 
-    // Update active class for pagination
-    var paginationItems = document.querySelectorAll('.page-item');
-    paginationItems.forEach(function (item) {
-        item.classList.remove('active');
-    });
+//     // Update active class for pagination
+//     var paginationItems = document.querySelectorAll('.page-item');
+//     paginationItems.forEach(function (item) {
+//         item.classList.remove('active');
+//     });
 
-    var activePaginationItem = document.querySelector('.page-item-' + pageNumber);
-    activePaginationItem.classList.add('active');
-}
+//     var activePaginationItem = document.querySelector('.page-item-' + pageNumber);
+//     activePaginationItem.classList.add('active');
+// }
 
 function myFunction() {
     var input, filter, table, tr, td, i, txtValue;
@@ -30,8 +30,8 @@ function myFunction() {
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
         a = tr[i].getElementsByTagName("a")[0];
-        if (div) {
-        txtValue = div.textContent || div.innerText;
+        if (a) {
+        txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             tr[i].style.display = "";
         } else {
